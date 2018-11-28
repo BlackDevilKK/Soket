808cafcf94a84022ebd4c77c8b987b3c8728cab9
徐佳 <1422941648@qq.com>
2018-11-28 15:04:30 +0800

    add tcp_client

---
+++ tcp_client.py
@@ -0,0 +1,12 @@
+import socket
+import sys
+def start_tcp_client(ip, port):
+  #server port and ip
+  server_ip = ip
+  servr_port = port
+  tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  try:
+    tcp_client.connect((server_ip, server_port))
+  except socket.error:
+    print("fail to setup socket connection")
+  tcp_client.close()
