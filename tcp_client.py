fe4dfba7d7c9c8ea9e8797f2bdda0945b964d84c
徐佳 <1422941648@qq.com>
2018-11-29 15:28:02 +0800

    修改error

--- tcp_client.py
+++ tcp_client.py
@@ -7,6 +7,8 @@ def start_tcp_client(ip, port):
   tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   try:
     tcp_client.connect((server_ip, server_port))
-  except socket.error:
+  except:
     print("fail to setup socket connection")
   tcp_client.close()
+  if __name__ == '__main__':
+    start_tcp_clien('192.168.0.107', 12345)
